dcc6829352b13dc4f4c0069ee56c18f74d1d865a
Jaume Vidal Torrents <jaume.vidal@enti.cat>
2024-04-29 13:16:58 +0200

    Animaciones muerte terminada

--- Assets/Scripts/character/HealthBar.cs
+++ Assets/Scripts/character/HealthBar.cs
@@ -8,29 +8,35 @@ public class HealthBar : MonoBehaviour
 {
     private Slider slider;
     public int actualHealthPlayer = 100;
+    [Header("Animacion")]
+    public Animator animator;
 
     
-
     public void Start()
     {
         slider = GetComponent<Slider>();
 
-        
-        
-    }
 
+    }
+    public void Update()
+    {
+        animator.SetInteger("Vida", actualHealthPlayer);
+    }
     public void ChangeMaxLife(float maxLife)
     {
         slider.value = maxLife;
+        
     }
 
     public void ChangeActualLife(float actualLife)
     {
         slider.value -= actualLife;
+        actualHealthPlayer =(int)slider.value;
     }
 
     public void InicializeHealthBar(float actualHealth)
     {
+        
         ChangeMaxLife(actualHealth);
         //ChangeActualLife(actualHealth);
     }
