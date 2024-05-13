240acf23a414874bee925e5a60d2cf4d8b3e65cc
Monke <gerard.busca@enti.cat>
2024-05-13 13:48:03 +0200

    Doors now work well

--- Assets/Scripts/objects/CoinManager.cs
+++ Assets/Scripts/objects/CoinManager.cs
@@ -6,14 +6,21 @@ public class CoinManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public int coinCount;
+    public int numCoinsToUnlock_door1;
+
+    public GameObject door1;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (coinCount >= numCoinsToUnlock_door1 && door1 != null)
+        {
+            Destroy(door1);
+        }
     }
 }
