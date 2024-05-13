3be2f43302e04ddfc129b48f95677e0ef037b516
Monke <gerard.busca@enti.cat>
2024-05-13 13:52:27 +0200

    Now we can support several doors

--- Assets/Scripts/objects/CoinManager.cs
+++ Assets/Scripts/objects/CoinManager.cs
@@ -6,9 +6,12 @@ public class CoinManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public int coinCount;
-    public int numCoinsToUnlock_door1;
+    public int numCoinsToUnlock_1;
+    public int numCoinsToUnlock_2;
+
 
     public GameObject door1;
+    public GameObject door2;
 
     void Start()
     {
@@ -18,9 +21,13 @@ public class CoinManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (coinCount >= numCoinsToUnlock_door1 && door1 != null)
+        if (coinCount >= numCoinsToUnlock_1 && door1 != null)
         {
             Destroy(door1);
+        }
+        if (coinCount >= numCoinsToUnlock_2 && door2 != null)
+        {
+            Destroy(door2);
         }
     }
 }
