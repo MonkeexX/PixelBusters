06409268d355f0d4204c0c8a61fbadf53d4e442c
HectorZ70 <hector.zornoza@enti.cat>
2024-05-31 10:14:11 +0200

    Boss fight

---
+++ Assets/Door.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
