3f763653929c4ea0fee65c75a748d2239a91b1e6
HectorZ70 <hector.zornoza@enti.cat>
2024-05-31 18:08:19 +0200

    Si

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float reconstructionTime = 1.0f;
+    public Collider2D door2Collider;
+    public SpriteRenderer door2Renderer;
     void Start()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        door2Collider = GetComponent<Collider2D>();
+        door2Renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -16,6 +21,21 @@ public class Door : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            if(gameObject == null)
+            {
+
+                StartCoroutine(ReconstructionAfterTime());
+            }
         }
+    }
+    private IEnumerator ReconstructionAfterTime()
+    {
+        door2Collider.enabled = false;
+        door2Renderer.enabled = false;
+        
+        yield return new WaitForSeconds(reconstructionTime);
+   
+        door2Collider.enabled = true;
+        door2Renderer.enabled = true;
     }
 }
